a788920319f25dfb061c0859d6e27c8f72b7f68d
lucas16lm <147983949+lucas16lm@users.noreply.github.com>
2026-03-14 20:11:26 +0100

    added water, characters, environment and animation packages

---
+++ Assets/Ship.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    [SerializeField] private float maxSpeed;
+
+    private float currentSpeed;
+    public void HoistSails()
+    {
+
+    }
+
+    public void LowerSails()
+    {
+
+    }
+}
